781e4aa043bb4d0a3e64dc7368c04ad685e0d6f0
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-08-28 01:34:00 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,8 @@
+a=raw_input("Enter Text :")
+b=raw_input("Enter Text :")
+if(a==b):
+    print("A and B are equal ")
+    print("Thank you for using our software")
+else :
+    print("A and B aren't equal")
+    printf("Thank you for using our software")
