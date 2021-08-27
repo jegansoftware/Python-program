ae01320f78f37424d9503f191a7db390b8edfca9
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-08-28 01:14:46 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,7 @@
+a=input("Enter A value :")
+b=input("Enter B value :")
+c=input("Enter S value :")
+k=(-b+((b*b)+(4*a*c))**0.5 /(2*a))
+print("------------------------")
+print("Answer is :")
+print(k)
