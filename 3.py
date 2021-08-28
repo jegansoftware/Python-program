14892125cef062ed08d2b099f00b6626639deebc
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-08-29 00:20:36 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,18 @@
+a=raw_input("Enter Text :")
+b=raw_input("Enter Text :")
+if(a==b):
+    print("A and B are equal ")
+    print("Thank you for using our software")
+else :
+    print("A and B aren't equal")
+    print("Thank you for using our software")
+
+s="Text No 1: is %s " %a
+ss="Text No 2: is %s" %b
+print "--------------------"
+print s
+print ss
+
+
+
+    
