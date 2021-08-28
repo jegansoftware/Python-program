f4039cc490c20e2dbe635021e29c6188c52e732b
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-08-29 01:11:41 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,17 @@
+a=raw_input("Enter Name   :")
+b=input("Enter Age  :")
+c=raw_input("Have you completed Degree ?")
+d=input("Enter Your Mobile No :")
+
+print
+print
+
+s="Name  : %s" %a
+s1="Age   : %s" %b
+s2="Degree: %s" %c
+s3="Phone : %s" %d
+print(s)
+print(s1)
+print(s2)
+print(s3)
+
