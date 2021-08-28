d8dabc32592a72bae30c854ceaed7400768c83d2
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-08-29 00:43:51 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,21 @@
+a=input("Enter A value :")
+b=input("Enter B value :")
+c=a+b
+d=a*b
+e=a-b
+f=a/b
+print("---------------")
+s1="Addition    : %d" %c
+s2="Subtraction : %d" %e
+s3="Multiplication: %d" %d
+s4="Division : %d "%f
+
+
+print s1
+print s2
+print s3
+print s4
+
+print("\nThe Program has been executed and gotten output successfully ");
+
+print("----------------")
